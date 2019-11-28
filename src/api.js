09426a91c8f8508f515ffe364831c1d92a8ed213
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-11-27 21:33:59 -0500

    Step 7 solution.

--- src/api.js
+++ src/api.js
@@ -16,7 +16,19 @@ const createItem = function (name) {
   });
 };
 
+const updateItem = function (id, updateData) {
+  const newData = JSON.stringify(updateData);
+  return fetch(`${BASE_URL}/items/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: newData
+  });
+};
+
 export default {
   getItems,
-  createItem
+  createItem,
+  updateItem
 };
